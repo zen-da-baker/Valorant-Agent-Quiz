bd394035df3f8159cd764ff57742ae629a379ccb
Don <120154112+zen-da-baker@users.noreply.github.com>
2024-07-03 08:24:35 -0700

    Stylesheet files created and added to html

--- app.js
+++ app.js
@@ -2,12 +2,16 @@
 import test from './modules/test.js';
 import { agents } from './modules/agentList.js';
 
+let agentDisplay = document.getElementById('agent-display');
+
 // Test for if the DOM can be manipulated by data from a module
 test.innerHTML = "hello";
 
 // Console test for if all agent objects are readable
 for (let i = 0; i < agents.length; i++) { 
     console.log(agents[i]);
+    agentDisplay.innerHTML = agents[i].name;
 }
 
 console.log(agents.length);
+
