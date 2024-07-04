ab1e609c41ed169797e4f757a247907467b87b82
Don <120154112+zen-da-baker@users.noreply.github.com>
2024-07-03 21:42:52 -0700

    quiz file created

---
+++ modules/quiz.js
@@ -0,0 +1,25 @@
+
+
+const question1 = {
+    msg: "What is your favorite color?",
+    choice1: {
+        text: "red",
+        trait: "fun"
+    },
+    choice2: {
+        text: "green",
+        trait: "classic"
+    }, 
+    choice3: {
+        text: "neon",
+        trait: "techie"
+    },
+    choice4: {
+        text: "orange",
+        trait: "serious"
+    }
+}
+
+const questions = [question1];
+
+let playerChoices = [];
