e9513a640bb86c6f74d661ae3c7533c1d9c1a73e
Don <120154112+zen-da-baker@users.noreply.github.com>
2024-07-03 22:06:05 -0700

    Question class created

--- modules/quiz.js
+++ modules/quiz.js
@@ -1,22 +1,54 @@
+class Question {
+    constructor(msg, text1, trait1, text2, trait2, text3, trait3, text4, trait4) {
+        this.msg = msg;
 
+        this.text1 = text1;
+        this.trait1 = trait1;
 
-const question1 = {
-    msg: "What is your favorite color?",
+        this.text2 = text2;
+        this.trait2 = trait2;
+
+        this.text3 = text3;
+        this.trait3 = trait3;
+
+        this.text4 = text4;
+        this.trait4 = trait4;
+    }
+}
+
+const question1 = new Question(
+    "What is your favorite color?",
+
+    "red",
+    "fun",
+
+    "green",
+    "classic",
+
+    "neon",
+    "techie",
+
+    "orange",
+    "serious"
+)
+
+const question = {
+    msg: "",
     choice1: {
-        text: "red",
-        trait: "fun"
+        text: "",
+        trait: ""
     },
     choice2: {
-        text: "green",
-        trait: "classic"
-    }, 
+        text: "",
+        trait: ""
+    },
     choice3: {
-        text: "neon",
-        trait: "techie"
+        text: "",
+        trait: ""
     },
     choice4: {
-        text: "orange",
-        trait: "serious"
+        text: "",
+        trait: ""
     }
 }
 
